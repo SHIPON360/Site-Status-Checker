8111aa034ffe66c133ca140351197f1c639680de
SHIPON360 <sheikhshipon231@gmail.com>
2026-05-29 00:56:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ CHAT_ID = "@cookieslinkserver"
 URL = "http://skysysx.net/e/boss"
 
 CHECK_INTERVAL = 2
-ALIVE_INTERVAL = 1800
+ALIVE_INTERVAL = 3600
 RETRY_LIMIT = 3
 
 # ============== STATE ==============
